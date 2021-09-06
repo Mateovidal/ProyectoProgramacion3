98cb3642bf5f6b2e15d8ad1ac9bdb0ed39692fa5
iestivill <iestivill@udesa.edu.ar>
2021-09-06 18:21:51 -0300

    navbar folder

---
+++ proyectoprogramacion3/src/components/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Navbar() {
+
+    return (
+
+        <footer>
+            <ul className="team">
+                <li>Ignacio Estivil</li>
+                <li>David Zir</li>
+                <li>Mateo Vidal</li>
+            </ul>
+        </footer>
+    
+    )
+
+}
+
+export default Navbar
