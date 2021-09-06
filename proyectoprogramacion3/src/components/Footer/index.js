991d1d7ac8f4bf0d345a114b23744aefb6c6dc38
Mateo Vidal <matevidal@hotmail.com.ar>
2021-09-06 11:15:08 -0300

    cambioa generales

---
+++ proyectoprogramacion3/src/components/Footer/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Footer() {
+
+    return (
+
+        <footer>
+            <ul className="team">
+                <li>Ignacio Estivil</li>
+                <li>David Zir</li>
+                <li>Mateo Vidal</li>
+            </ul>
+        </footer>
+    
+    )
+
+}
+
+export default Footer
---
+++ proyectoprogramacion3/src/components/Header/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Header() {
+
+    return (
+
+    <header>
+        <h1>Chumlify</h1>
+        <section>
+            <p>Ordenar ASC/ DESC</p>
+            <i className="fas fa-th"></i>
+            <i className="fas fa-align-justify"></i>
+            <form action="">
+                <input type="text" name="search" id="" placeholder="Search"/>
+                <button type="submit"><i className="fas fa-search"></i></button>
+            </form>
+        </section>
+    </header>
+    )
+
+}
+
+
+export default Header
