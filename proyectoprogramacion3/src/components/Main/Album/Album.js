c8d114ec49fc02adf031ca72957026d6e2935395
iestivill <iestivill@udesa.edu.ar>
2021-09-12 17:18:34 -0300

    fixing errors 1

--- proyectoprogramacion3/src/components/Main/Album/Album.js
+++ proyectoprogramacion3/src/components/Main/Album/Album.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Card from './Card/Card'
 
+
+
+
 class Album extends Component {
     constructor(){
         super()
@@ -9,9 +12,10 @@ class Album extends Component {
         }
     }
 
+    
     componentDidMount(){
         console.log('componentDidMount');
-        const url = "https://thingproxy.freeboard.io/fetch/https://api.deezer.com/chart/0/albums&top?limit=10";
+        const url = "https://thingproxy.freeboard.io/fetch/https://api.deezer.com/chart/0/albums&top?limit=32";
         fetch(url)
         .then((respuesta) => respuesta.json())
         .then((data) => {
@@ -24,15 +28,36 @@ class Album extends Component {
         .catch(e => console.log(e))
     }
 
+    agregarCards(){
+
+
+        // let url = this.state.nextUrl;
+        // fetch(url)
+        //     .then((response) => response.json())
+        //     .then((data) => {
+        //         console.log(data);
+        //         this.setState({
+
+    
+        //             // personajes: this.state.personajes.concat(data.results),
+        //             // nextUrl: data.info.next,  //Para tener la página siguiente.
+        //         })
+        //     })
+        //     .catch( function (e){
+        //         console.log(e);
+        //     })
+    }
+
     render() {
         return (
             <>
-              
+              <button  className="agregarCards"> Add Cards</button>
+              <div>
                 {this.state.albums.map((album) => (
                     <Card key={album.id} datosAlbum={album} />
 
                 ))}    
-
+            </div>
             </>
         )
     }
--- proyectoprogramacion3/src/components/Main/Main.js
+++ proyectoprogramacion3/src/components/Main/Main.js
@@ -21,25 +21,7 @@ componentDidMount(){
 
 
 // creamos el metodo agregarCards
-agregarCards(){
 
-
-            let url = this.state.nextUrl;
-            fetch(url)
-                .then((response) => response.json())
-                .then((data) => {
-                    console.log(data);
-                    this.setState({
-
-        
-                        // personajes: this.state.personajes.concat(data.results),
-                        // nextUrl: data.info.next,  //Para tener la página siguiente.
-                    })
-                })
-                .catch( function (e){
-                    console.log(e);
-                })
-        }
 
         // pasarle desde app propiedades a main
      
@@ -50,7 +32,7 @@ render(){
     <main>
 
                          {/* para llamar a un metodo como estamos hablando del propio componente usamos this. y el nombre del metodo agregarCards */}
-        <button onClick={()=>this.agregarCards()} className="agregarCards"> Add Cards</button>
+      
         <section className='card-container'>
 
             <Album />
