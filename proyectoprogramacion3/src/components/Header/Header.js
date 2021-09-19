83055f9a14aaccf5c3463987a15168fbc7549067
Mateo Vidal <matevidal@hotmail.com.ar>
2021-09-18 22:56:08 -0300

    falso commit para arreglar mi branch

--- proyectoprogramacion3/src/components/Header/Header.js
+++ proyectoprogramacion3/src/components/Header/Header.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 import './header.css'
 
-// import Search from '../Main/Album/Search/Search';
+// import Search from '../Main/Album/Search/Search'
+
 
 function Header() {
 
@@ -28,42 +29,4 @@ function Header() {
 
 }
 
-export default Header
-
-
-// import React, { Component } from 'react'
-
-// class Header extends Component {
-
-//     constructor() {
-//         super()
-//         this.state = {
-//             albums: [],
-//         }
-//     }
-
-
-//     filtrarAlbums(){
-
-//     }
-
-//     render() {
-//         return (
-//             <>
-//                 <nav className="navbar">
-//                     <h2 className="navbarTitle">Chumlify</h2>
-
-//                     <section className='navbarSection'>
-//                         <p className="navbarItem1">Ordenar ASC/ DESC</p>
-//                         <img src="https://img.flaticon.com/icons/png/512/81/81484.png?size=1200x630f&pad=10,10,10,10&ext=png&bg=FFFFFFFF" alt='' className="navbarItem2"></img>
-//                         <img src="https://img.flaticon.com/icons/png/512/81/81484.png?size=1200x630f&pad=10,10,10,10&ext=png&bg=FFFFFFFF" alt='' className="navbarItem2"></img>
-//                         <Search filtrarAlbums={(param)=> this.filtrarAlbums(param)} />
-//                     </section>
-//                 </nav>
-                
-//             </>
-//         )
-//     }
-// }
-
-// export default Header
+export default Header;
