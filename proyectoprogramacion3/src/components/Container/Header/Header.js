fed7c251feb1a3b9c490444da4e17c0535422b38
David <dzircampero@udesa.edu.ar>
2021-09-19 21:51:26 -0300

    limpieza header

--- proyectoprogramacion3/src/components/Container/Header/Header.js
+++ proyectoprogramacion3/src/components/Container/Header/Header.js
@@ -1,8 +1,6 @@
 import React, {Component} from 'react';
-
 import './header.css'
 
-// import Search from '../Main/Album/Search/Search';
 
 class Header extends Component {
 
@@ -53,50 +51,9 @@ class Header extends Component {
                 </form>
             </div>
         </nav>
-        
- 
-    
     )
     }
 
 }
 
 export default Header
-
-
-// import React, { Component } from 'react'
-
-// class Header extends Component {
-
-//     constructor() {
-//         super()
-//         this.state = {
-//             albums: [],
-//         }
-//     }
-
-
-//     filtrarAlbums(){
-
-//     }
-
-//     render() {
-//         return (
-//             <>
-//                 <nav className="navbar">
-//                     <h2 className="navbarTitle">Chumlify</h2>
-
-//                     <section className='navbarSection'>
-//                         <p className="navbarItem1">Ordenar ASC/ DESC</p>
-//                         <img src="https://img.flaticon.com/icons/png/512/81/81484.png?size=1200x630f&pad=10,10,10,10&ext=png&bg=FFFFFFFF" alt='' className="navbarItem2"></img>
-//                         <img src="https://img.flaticon.com/icons/png/512/81/81484.png?size=1200x630f&pad=10,10,10,10&ext=png&bg=FFFFFFFF" alt='' className="navbarItem2"></img>
-//                         <Search filtrarAlbums={(param)=> this.filtrarAlbums(param)} />
-//                     </section>
-//                 </nav>
-                
-//             </>
-//         )
-//     }
-// }
-
-// export default Header
